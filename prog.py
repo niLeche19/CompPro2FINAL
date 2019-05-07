e0c37df6eb5efa738c1e48e9782a18e2fb8fbe48
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2019-05-06 21:32:52 -0400

    Update prog.py

--- prog.py
+++ prog.py
@@ -1,15 +1,13 @@
-from pynput.keyboard import Key, Listener
+from pynput.keyboard import Key, Listener, Controller
 from sys import exit
 import pyautogui as pag
+import winsound
 
+quit = []
 pressed = []
 good = 1
 
-def prlst():
-    while True:
-        print(pressed)
-
-# is called when key is released and removes key from Pressed
+# is called when key is released and removes said key from Pressed
 def isdown(kiki):
     try:
         for i in range(len(pressed)):
@@ -21,12 +19,22 @@ def isdown(kiki):
 
 # press the key again
 def dblprs(k):
-    if k.char == 'q' or k.char == 'Q':
-        listener.stop()
     pressed.append(k)
     pag.press(k.char)
 
 def on_press(key):
+    # press both shifts to quit program
+    if key == Key.shift_l:
+        if len(quit) != 0:
+            listener.stop()
+        else:
+            quit.append('0')
+
+    if key == Key.shift_r:
+        if len(quit) != 0:
+            listener.stop()
+        else:
+            quit.append('1')
     # checks if key is pressed from list, dblprs adds keys to avoid infinte loops.
     try:
         if len(pressed) > 0:
@@ -36,18 +44,19 @@ def on_press(key):
                     good = 0
         else:
             good = 1
-    # makes sure that a doublepress is not occurring and presses accordingly.
+    # makes sure the coast is clear and presses.
         if good == 1:
-            print('pressing!!')
             pressed.append(key)
             dblprs(key)
     except:
         pass
 
 def on_release(key):
+    if key == Key.shift_l or key == Key.shift_r:
+        del quit[:]
     isdown(key)
 
-# defines and starts the built in Listener with Pynput
+# Defines and starts Pynput's built in Listener function.
 with Listener(
         on_press=on_press,
         on_release=on_release) as listener:
