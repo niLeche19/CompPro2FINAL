bf09d3ad09a1aaeac4db230ea0619d49b35daa5c
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2019-05-14 11:00:49 -0400

    Updated from Brython Server: 5/14/2019 11:00:51 AM

--- 5_14.py
+++ 5_14.py
@@ -1,6 +1,6 @@
 aray = []
 
-for i in range(100):
+for i in range(2500):
     aray.append(i)
 
 def addall(s,n):
@@ -9,4 +9,42 @@ def addall(s,n):
     else:
         print(n)
 
-addall(aray,0)
+def SUM(n):
+    a = 0
+    for i in range(n):
+        a += i
+    print(a)
+
+def FIBO(n):
+    if n == 0:
+        return(1)
+    if n == 1:
+        return(1)
+    else:
+        return (FIBO(n-1) + FIBO(n-2))
+
+def fibo(l,n):
+    lst = l
+    if n+2 <= len(lst):
+        for i in l:
+            print(i)
+    else:
+        lst.append(lst[len(lst)-1] + lst[len(lst)-2])
+        fibo(l, n-1)
+
+# fibo([1,1], 30)
+# print(FIBO(10))
+
+
+
+
+if d <= 1:
+        draw_point(x, y)                 # base case
+    else:
+        # recurse on upper left quadrant
+        draw_sierpinsky(x, y, d/2)
+        # recurse on upper right quadrant
+        draw_sierpinsky(x + d/2, y, d/2)
+        # recurse on lower right quadrant
+        draw_sierpinsky(x + d/2, y + d/2, d/2)
+        
